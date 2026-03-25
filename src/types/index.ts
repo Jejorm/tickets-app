52b09662bfc8e4793eb3da10b49f597d85f924fc
Jeremy Orellana <jejorm8@gmail.com>
2026-03-25 12:01:40 -0500

    feat: Define explicit `ServerMessage` and `ClientMessage` types for clearer WebSocket communication.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,27 +1,35 @@
+import type { QueueMessageState } from './queue-message-state'
+import type { Ticket } from './tickets'
+
+//! Este es el objeto que envía el servidor
+export type ServerMessage =
+	| { type: 'ERROR'; payload: { error: string } }
+	| {
+			type: 'QUEUE_STATE'
+			payload: {
+				state: QueueMessageState
+			}
+	  }
+	| { type: 'TICKET_CREATED'; payload: { ticket: Ticket } }
+	| {
+			type: 'NEXT_TICKET_ASSIGNED'
+			payload: { ticket?: Ticket }
+	  }
+	| {
+			type: 'QUEUE_EMPTY'
+	  }
+
+//! Este es el objeto que recibe el servidor
+export type ClientMessage =
+	| { type: 'GET_STATE' }
+	| { type: 'CREATE_TICKET'; payload: { isPreferential: boolean } }
+	| {
+			type: 'REQUEST_NEXT_TICKET'
+			payload: { deskNumber: number; forceNormalTicket: boolean }
+	  }
+	| { type: 'RESET_QUEUE' }
+
 //! Este es el objeto que se almacena por cada cliente
 export interface WebSocketData {
 	clientId: string
 }
-
-//! Este es el objeto que recibe el servidor
-export interface WebSocketMessage {
-	type: MessageType
-	payload: unknown
-}
-
-export type MessageType = 'PERSONAL_MESSAGE' | 'BROADCAST_MESSAGE'
-
-//! Este es el objeto que se envía al cliente
-export interface WebSocketResponse {
-	type: ResponseType
-	payload: unknown
-}
-
-export type ResponseType =
-	| 'ERROR'
-	| 'PERSONAL_RESPONSE_MESSAGE'
-	| 'BROADCAST_RESPONSE_MESSAGE'
-	| 'ITEM_ADDED'
-	| 'ITEM_UPDATED'
-	| 'ITEM_DELETED'
-	| 'ITEMS_LIST'
